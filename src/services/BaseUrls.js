0ab98cb1cfa9d77b75c614b5913b036568871c02
brian mwema <brianwgatundu@gmail.com>
2023-10-31 03:04:57 +0300

    debugging

---
+++ src/services/BaseUrls.js
@@ -0,0 +1,15 @@
+// export const userUrl = "http://127.0.0.1:8000/auth/";
+// export const cartBaseUrl = "http://127.0.0.1:8000/menu/";
+// export const mpesaBaseUrl = "http://127.0.0.1:8000/mpesa/";
+// export const chatApi = "http://127.0.0.1:8000/chat";
+// export const profileApi = "http://127.0.0.1:8000/profile/";
+export const userUrl = "https://testmanage-20ef9d49fe9a.herokuapp.com/auth/";
+export const cartBaseUrl =
+  "https://testmanage-20ef9d49fe9a.herokuapp.com/menu/";
+export const mpesaBaseUrl =
+  "https://testmanage-20ef9d49fe9a.herokuapp.com/mpesa/";
+export const chatApi = "https://testmanage-20ef9d49fe9a.herokuapp.com/chat";
+export const profileApi =
+  "https://testmanage-20ef9d49fe9a.herokuapp.com/profile/";
+export const charts =
+  "https://testmanage-20ef9d49fe9a.herokuapp.com/records/api/dailyrecord//";
